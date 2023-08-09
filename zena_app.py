79b049d7bf740711babe181a5a54b46ae30ad8d0
sdutta24 <81972601+sdutta24@users.noreply.github.com>
2023-08-09 10:00:02 +0530

    Update zena_app.py
    update2

--- zena_app.py
+++ zena_app.py
@@ -5,3 +5,7 @@ streamlit.title('Zena\'s Amazing Athleisure Catalog')
 #Connect to snowflake
 my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur=mycnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
